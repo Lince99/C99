aeb0d6eacef59c30eb5d517e3c21af1ec11262af
Lince99 <lynxnic99@gmail.com>
2019-04-12 22:34:03 +0200

    Fix Warning on compile for a TODO save/load function var

--- CursesGraphic/AsciiArt/ascii_art.c
+++ CursesGraphic/AsciiArt/ascii_art.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) {
     //user input var
     int ch = 0;
     int usr_req = 0;
-    char* usr_str = NULL;
+    //char* usr_str = NULL;
     int** matrix = NULL;
     int mat_y = 0;
     int mat_x = 0;
